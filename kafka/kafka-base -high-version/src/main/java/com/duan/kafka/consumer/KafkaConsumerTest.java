d1b1d8dae2d0a588b76ce1ae617eba4b12a3b232
iTommyChi <iTommychi@foxmail.com>
2019-10-17 15:44:05 +0800

    use kafka new api --kafka_2.12_2.1.0

---
+++ kafka/kafka-base -high-version/src/main/java/com/duan/kafka/consumer/KafkaConsumerTest.java
@@ -0,0 +1,31 @@
+package com.duan.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaConsumerTest {
+
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "duan1:9092,duan2:9092,duan3:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("nongfu"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ kafka/kafka-base -high-version/src/main/java/com/duan/kafka/producer/KafkaProducerTest.java
@@ -0,0 +1,39 @@
+package com.duan.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaProducerTest {
+    //http://kafka.apache.org/23/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "duan1:9092,duan2:9092,duan3:9092");
+        props.put("transactional.id", "my-transactional-id");
+        Producer<String, String> producer = new KafkaProducer<>(props, new StringSerializer(), new StringSerializer());
+
+        producer.initTransactions();
+
+        try {
+            producer.beginTransaction();
+            int j = 0;
+            for (int i = 0; i < 10000; i++) {
+                producer.send(new ProducerRecord<>("nongfu", Integer.toString(i), "hello--"+Integer.toString(i)));
+                j++;
+                System.out.println("我是里："+i);
+                Thread.sleep(500);
+            }
+            System.out.println("我是外："+j);
+            producer.commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+            producer.close();
+        }
+        producer.close();
+    }
+}
